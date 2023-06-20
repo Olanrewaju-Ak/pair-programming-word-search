5c1a27cd08a7fb199251c0786260d81eeed65b2e
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-19 21:20:34 -0700

    create wordsearch function to check for words in vertical , horizontal or backward order

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  const result = [];
 
-module.exports = wordSearch
+  for (let j = 0; j < columns; j++) {
+    const newRow = [];
+    for (let i = 0; i < rows; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    result.push(newRow);
+  }
+
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+
+  const horizontalReverse = letters.map((ls) => ls.reverse().join(""));
+  const verticalReverse = letters.map((ls) => ls.reverse().join(""));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      console.log(true);
+      return true;
+    }
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      console.log(true);
+      return true;
+    }
+  }
+  for (l of horizontalReverse) {
+    if (l.includes(word)) {
+      console.log(true);
+      return true;
+    }
+  }
+  for (l of verticalReverse) {
+    if (l.includes(word)) {
+      console.log(true);
+      return true;
+    }
+  }
+
+  console.log(false);
+  return false;
+};
+
+module.exports = wordSearch;
